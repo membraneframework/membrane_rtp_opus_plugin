022802f77a6ad3e3b24a7ff586c3d63a6a96812c
Marek Kaput <marek.kaput@swmansion.com>
2020-03-24 10:00:32 +0100

    scaffold project structure

---
+++ mix.exs
@@ -0,0 +1,66 @@
+defmodule Membrane.RTP.Opus.Mixfile do
+  use Mix.Project
+
+  @version "0.1.0"
+  @github_url "https://github.com/membraneframework/membrane_rtp_opus"
+
+  def project do
+    [
+      app: :membrane_rtp_opus,
+      version: @version,
+      elixir: "~> 1.10",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+
+      # hex
+      description: "Membrane Multimedia Framework (RTP Opus)",
+      package: package(),
+
+      # docs
+      name: "Membrane: RTP Opus",
+      source_url: @github_url,
+      homepage_url: "https://membraneframework.org",
+      docs: docs()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: []
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_env), do: ["lib"]
+
+  defp deps do
+    [
+      {:membrane_core, "~> 0.5.0"},
+      {:membrane_caps_rtp, "~> 0.1"},
+      {:membrane_opus_format, "~> 0.2.0"},
+      {:ex_doc, "~> 0.21.0", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Membrane Team"],
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => @github_url,
+        "Membrane Framework Homepage" => "https://membraneframework.org"
+      }
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md"],
+      source_ref: "v#{@version}",
+      nest_modules_by_prefix: [Membrane.RTP.Opus]
+    ]
+  end
+end
