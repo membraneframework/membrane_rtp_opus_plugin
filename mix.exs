9cd823eeb6939ae363bc60f10271e666e1ee1317
AndrzejPodo <andrzejpodobinski2@gmail.com>
2021-11-18 10:57:51 +0100

    bump to 0.4.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RTP.Opus.Mixfile do
   use Mix.Project
 
-  @version "0.3.0"
+  @version "0.4.0"
   @github_url "https://github.com/membraneframework/membrane_rtp_opus_plugin"
 
   def project do
@@ -37,7 +37,7 @@ defmodule Membrane.RTP.Opus.Mixfile do
 
   defp deps do
     [
-      {:membrane_core, "~> 0.7.0"},
+      {:membrane_core, "~> 0.8.0"},
       {:membrane_rtp_format, "~> 0.3.0"},
       {:membrane_opus_format, "~> 0.3.0"},
       {:ex_doc, "~> 0.24.0", only: :dev, runtime: false},
