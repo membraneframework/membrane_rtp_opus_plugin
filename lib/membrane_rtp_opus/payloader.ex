1107a5e6221f979f45cbf26b3b74896087e3c94b
Jan Izydorczyk <jan.izydorczyk@swmansion.com>
2022-12-21 14:02:54 +0100

    Fix formatting

--- lib/membrane_rtp_opus/payloader.ex
+++ lib/membrane_rtp_opus/payloader.ex
@@ -14,7 +14,8 @@ defmodule Membrane.RTP.Opus.Payloader do
       any_of(
         %Opus{self_delimiting?: false},
         %RemoteStream{type: :packetized, content_format: content_format}
-      when content_format in [Opus, nil]),
+        when content_format in [Opus, nil]
+      ),
     demand_unit: :buffers
 
   def_output_pad :output, accepted_format: RTP
