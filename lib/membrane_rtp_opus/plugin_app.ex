b91f03775a675252d404eb232ac820b76c572452
noarkhh <jakub.pryc@gmail.com>
2024-12-12 16:59:56 +0100

    Rename opus encoding name to match rtpmap value

--- lib/membrane_rtp_opus/plugin_app.ex
+++ lib/membrane_rtp_opus/plugin_app.ex
@@ -7,14 +7,14 @@ defmodule Membrane.RTP.Opus.Plugin.App do
           {:ok, pid} | {:error, {:already_started, pid} | {:shutdown, term} | term}
   def start(_type, _args) do
     PayloadFormat.register(%PayloadFormat{
-      encoding_name: :OPUS,
+      encoding_name: :opus,
       payload_type: 120,
       payloader: Opus.Payloader,
       depayloader: Opus.Depayloader,
       frame_detector: fn _payload -> true end
     })
 
-    PayloadFormat.register_payload_type_mapping(120, :OPUS, 48_000)
+    PayloadFormat.register_payload_type_mapping(120, :opus, 48_000)
     Supervisor.start_link([], strategy: :one_for_one, name: __MODULE__)
   end
 end
