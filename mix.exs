ff8fa0af2cd953dd17a0dee8a71b5f912cadc08c
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2026-05-05 14:21:49 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RTP.Opus.Mixfile do
   use Mix.Project
 
-  @version "0.10.1"
+  @version "0.10.2"
   @github_url "https://github.com/membraneframework/membrane_rtp_opus_plugin"
 
   def project do
