66435ebc150528486e1ed70c541f32b8865217fb
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2026-06-29 11:27:53 +0200

    Set ex_doc dependency to >= 0.40.0

--- mix.exs
+++ mix.exs
@@ -42,7 +42,7 @@ defmodule Membrane.RTP.Opus.Mixfile do
       {:membrane_core, "~> 1.0"},
       {:membrane_rtp_format, "~> 0.11.0"},
       {:membrane_opus_format, "~> 0.3.0"},
-      {:ex_doc, "~> 0.40", only: :dev, runtime: false},
+      {:ex_doc, ">= 0.40.0", only: :dev, runtime: false},
       {:dialyxir, "~> 1.4", only: :dev, runtime: false},
       {:credo, "~> 1.7", only: :dev, runtime: false}
     ]
